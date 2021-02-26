a497e5e7973783c316145ac4fd14a0cf9d920e91
Jason M. Pittman <jason@jasonmpittman.com>
2021-02-26 09:46:25 -0500

    updated utz.py

--- utz.py
+++ utz.py
@@ -13,6 +13,12 @@ import configparser
 
 class utz:
 
+    __AMOUNTS = {
+        "KB": 1,
+        "MB": 2,
+        "GB": 3
+    }
+
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
@@ -21,19 +27,40 @@ class utz:
         self.__interface = config['options']['net_interface']
 
     def cpu(self):
-	    return psutil.cpu_percent(float(self.__interval), True)
+        """returns percent utilization on all CPU cores as a tuple"""
+        return psutil.cpu_percent(float(self.__interval), True)
 
-    def ram(self):
+    def ram(self, amount=None):
+        """returns amount of RAM used in GB by default. KB or MB can be passed for different amounts"""
         mem = psutil.virtual_memory()
         used = mem[3]
-        return (used / 1024 / 1024 / 1024) #KB, MB, GB
 
-    def disk(self):
+        if amount is None:
+            return self.__convertAmount(used, self.__AMOUNTS.get("GB"))
+        else:
+            return self.__convertAmount(used, self.__AMOUNTS.get(amount))
+        
+
+    def disk(self, amount=None):
+        """returns amount of disk free in GB by default. KB or MB can be passed for different amounts"""
         space = psutil.disk_usage('/')
         free = space[2]
-        return (free / 1024 / 1024 / 1024)
+        
+        if amount is None:
+            return self.__convertAmount(free, self.__AMOUNTS.get("GB"))
+        else:
+            return self.__convertAmount(free, self.__AMOUNTS.get(amount))
+        
 
     def net(self):
+        """returns network counters for interface specified in config.ini"""
         networks = psutil.net_io_counters(True)
           
         return networks.get(self.__interface)
+
+    def __convertAmount(self, value, amount):
+        """private method to convert bytes to specified amount"""
+        for i in range(amount):
+            value = (value / 1024)
+        
+        return value
